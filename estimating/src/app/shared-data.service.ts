18a8c54baf3e49f74159a32163321498b8128db6
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-06 17:35:48 -0600

    started working on service

--- estimating/src/app/shared-data.service.ts
+++ estimating/src/app/shared-data.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@angular/core';
 
+export interface Opponents {
+  name: string;
+}
+
+export interface Games {
+  opponent: string,
+  won: boolean,
+  lost: boolean
+}
+
 @Injectable({
   providedIn: 'root'
 })
