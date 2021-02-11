819bd05a3e6ce6e3a508fec095a7243a3627355d
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-10 20:45:33 -0600

    ngModel
    clearing text input with ngModel

--- estimating/src/app/app.module.ts
+++ estimating/src/app/app.module.ts
@@ -7,11 +7,11 @@ import { FooComponent } from './foo/foo.component';
 import { BarComponent } from './bar/bar.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatListModule} from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
