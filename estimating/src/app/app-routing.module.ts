86f32a7eafeb67d83da122ca9971c485549b3162
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-11 22:04:39 -0600

    routing not working...yet

--- estimating/src/app/app-routing.module.ts
+++ estimating/src/app/app-routing.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { FooComponent } from './foo/foo.component';
 import { BarComponent } from './bar/bar.component';
 
 const routes: Routes = [
-  // {
-  //   path: ""
-  //   , component: AppComponent 
-  // }
   {
-    path: "foo"
+    path: "app-foo"
     , component: FooComponent 
   }
   , {
-    path: "bar"
+    path: "app-bar"
     , component: BarComponent 
   }
 ];
